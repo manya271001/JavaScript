54ff9f6e7d5798f6a050f176d274569cb5ade251
Manya Sharma <manya271001@gmail.com>
2024-09-17 12:46:15 +0530

    input from user

---
+++ 17aug/finction.js
@@ -0,0 +1,22 @@
+// console.log("second class");
+
+// function first(){
+//     console.log("function called")
+//     let num = 90;
+//     let num2 = 100;
+//     console.log(num+num2);
+// }
+// first();
+// FUNCTION TO TAKE INPUT FROM USER
+ function first(){
+    console.log("function called")
+    let num = parseInt(window.prompt("enter number 1"))
+    console.log(num)
+    console.log(typeof(num))
+     let num2 = parseInt(window.prompt("enter number 2"))
+    console.log(num2)
+    console.log(typeof(num2))
+    console.log(num + num2)
+   
+}
+first();
