fc1543f4232f469fab1ced25f840a419d4ed648b
Manya Sharma <manya271001@gmail.com>
2024-09-18 11:56:31 +0530

    second commit of 18sep

---
+++ 18sep/script.js
@@ -0,0 +1,6 @@
+function class3(){
+    let num1 = parseInt(window.prompt("enter number 1"));
+    let num2 = parseInt(window.prompt("enter number 2"));
+    window.alert(num1 + num2);
+}
+class3();
