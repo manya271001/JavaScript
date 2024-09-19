0eb94c43446a5452bdfae09ae6b4336eb37d9ab3
Manya Sharma <manya271001@gmail.com>
2024-09-19 19:07:14 +0530

    final commit

--- 19sep/script2.js
+++ 19sep/script2.js
@@ -1,10 +1,11 @@
 function even_odd(){
     let num = parseInt(window.prompt("enter a number"))
-    if(num % 2 === 0){
-        window.alert("even")
+    if(num % 2 === 0)  // in JS we use 3 times = for comparison
+    {
+        window.alert("even" + num)
     }
     else {
-        window.alert("odd")
+        window.alert("odd" + num)
     }
 }
 even_odd()
