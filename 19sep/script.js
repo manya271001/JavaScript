5a67ebf8ba12d68d92bec8c2683b9bcd19e82e94
Manya Sharma <manya271001@gmail.com>
2024-09-19 12:12:56 +0530

    new window.location.href for going to another page

--- 19sep/script.js
+++ 19sep/script.js
@@ -1,7 +1,8 @@
 function check(){
     let res = window.confirm("do you really want to logout")
     if(res){
-        window.alert("okay logging out")
+        // window.alert("okay logging out")
+        window.location.href="login.html"
     }
     else {
         window.alert("wrong input")
