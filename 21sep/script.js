3d0c1d4cd24e7f093d85b618b3cc3bc0147cab0f
Manya Sharma <manya271001@gmail.com>
2024-09-21 12:20:59 +0530

    styleing to element

--- 21sep/script.js
+++ 21sep/script.js
@@ -1,6 +1,11 @@
 function dom(){
     let a = document.querySelector("#heading");
+    // applying styling
+    a.style.color="blue";
+    a.style.fontSize= "80px";
     let b = document.querySelector("#text");
-    console.log(a);
-    console.log(b);
+    b.style.color="red";
+    b.style.fontSize = "20px"
+    // console.log(a);
+    // console.log(b);
 }
