441243b8ed75ee3e3175e91d53ef16dc4d835ee6
Manya Sharma <manya271001@gmail.com>
2024-09-25 21:57:37 +0530

    made image chnage like on websites

---
+++ 25sep/product.js
@@ -0,0 +1,16 @@
+function change1(){
+    let slectedimg = document.querySelector("#largeimg");
+    slectedimg.src="https://m.media-amazon.com/images/I/61VuVU94RnL._SL1500_.jpg"
+}
+function change2(){
+    let slectedimg = document.querySelector("#largeimg");
+    slectedimg.src="https://m.media-amazon.com/images/I/61cecOpZrxL._SL1500_.jpg"
+}
+function change3(){
+    let slectedimg = document.querySelector("#largeimg");
+    slectedimg.src="https://m.media-amazon.com/images/I/61YfilUZTUL._SL1500_.jpg"
+}
+function change4(){
+    let slectedimg = document.querySelector("#largeimg");
+    slectedimg.src="https://m.media-amazon.com/images/I/818a-pz0BvL._SL1500_.jpg"
+}
