9f8c53a70382849a06fbed9b45ad0977464bc2ab
Manya Sharma <manya271001@gmail.com>
2024-09-25 12:09:25 +0530

    changed ul style on click

---
+++ 25sep/script.js
@@ -0,0 +1,9 @@
+// function run(){
+//     let selectedphoto = document.querySelector("#photo")
+//     selectedphoto.src= "./blog6.jpg"
+// }
+function run(){
+    let selectedul = document.querySelector("ul")
+        selectedul.type="square"
+    
+}
