0498e956c5642014d81d71ea631696542fa35681
Manya Sharma <manya271001@gmail.com>
2024-09-26 12:59:52 +0530

    taking user input

--- 26 sep/userinput.js
+++ 26 sep/userinput.js
@@ -1,8 +1,16 @@
 function data(){
     let selectedname = document.querySelector('#name')
     let name = selectedname.value;
+    if(name ===""){
+        window.alert("enter name")
+        return false;
+    }
     let selectedage = document.querySelector("#age")
     let age = selectedage.value;
+    if(age ===""){
+        window.alert("enter age")
+        return false;
+    }
    
     console.log(name)
      console.log(age);
