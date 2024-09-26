30a46bcf8e0d61a397ad4383e9b3c350bce07a88
Manya Sharma <manya271001@gmail.com>
2024-09-26 12:11:54 +0530

    first commit of 26 sep

---
+++ 26 sep/userinput.js
@@ -0,0 +1,11 @@
+function data(){
+    let selectedname = document.querySelector('#name')
+    let name = selectedname.value;
+    let selectedage = document.querySelector("#age")
+    let age = selectedage.value;
+   
+    console.log(name)
+     console.log(age);
+    return false;
+
+}
