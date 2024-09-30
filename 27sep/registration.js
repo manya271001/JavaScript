43c69f83f3057c979c242b956e7072d07245dbdb
Manya Sharma <manya271001@gmail.com>
2024-09-30 12:13:56 +0530

    1st 30 sep commit

--- 27sep/registration.js
+++ 27sep/registration.js
@@ -14,39 +14,63 @@ function data(){
          selectedname.style.outlineColor="red";
         return false;
     }
-    if(secondName===""){
+     else if(secondName===""){
         document.querySelector("#errorsecondname").innerHTML="enter second name"
         let selectedname=document.querySelector("#secondName")
         selectedname.style.borderColor="red"
         selectedname.style.outlineColor="red"
         return false;
     }
-    if(email===""){
+     else if(email===""){
      document.querySelector("#erroremail").innerHTML ="enter email please"
      let selectedemail = document.querySelector("#email")
      selectedemail.style.borderColor="red"
      selectedemail.style.outlineColor="red"
      return false;
     }
-    if(password ===""){
+     else if(password ===""){
         document.querySelector("#errorpassword").innerHTML="enter passowrd"
         let selectedpass = document.querySelector("#password");
         selectedpass.style.borderColor ="red"
         selectedpass.style.outlineColor = "red"
         return false;
     }
-    if(confirmpassword===""){
+     else if(confirmpassword===""){
         document.querySelector("#errorCpass").innerHTML="enter cofirm password"
         document.querySelector('#confirmpassword').style.borderColor="red"
         document.querySelector('#confirmpassword').style.outlineColor="red"
         return false;
     }
-    if(number===""){
+     else if(number===""){
         document.querySelector("#errorphone").innerHTML="enter phone number"
         document.querySelector("#number").style.borderColor="red"
         document.querySelector("#number").style.outlineColor="red"
         return false;
     }
-
-
+}
+function s(){
+    let selectedinput=document.querySelector('input')
+     selectedinput.style.borderColor="rgb(118, 118, 118) "
+    selectedinput.style.outlineColor="rgb(118, 118, 118)"
+    selectedinput.style.borderWidth="1.5px"
+    selectedinput.style.borderRadius="1.5PX"
+        let selectederrorsecondname=document.querySelector("#errorsecondname")
+    selectederrorsecondname.innerHTML="";
+    selectedinput.style.borderColor="rgb(118, 118, 118) "
+    selectedinput.style.outlineColor="rgb(118, 118, 118)"
+    selectedinput.style.borderWidth="1.5px"
+    selectedinput.style.borderRadius="1.5PX"
+    let selectederroremail=document.querySelector("#erroremail")
+    selectederroremail.innerHTML="";
+    selectedinput.style.borderColor="rgb(118, 118, 118) "
+    selectedinput.style.outlineColor="rgb(118, 118, 118)"
+    selectedinput.style.borderWidth="1.5px"
+    selectedinput.style.borderRadius="1.5PX"
+    let selectederrorpassword=document.querySelector("#errorpassword")
+    selectederrorpassword.innerHTML="";
+    let selectederrorCpass=document.querySelector("#errorCpass")
+    selectederrorCpass.innerHTML="";
+    let selectederrorphone=document.querySelector("#errorphone")
+    selectederrorphone.innerHTML="";
+   
 }
