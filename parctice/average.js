7ada27520e6f82acfd7c61215e4a76b7c9a257b8
Manya Sharma <manya271001@gmail.com>
2024-10-06 18:45:47 +0530

    2 practice questions

---
+++ parctice/average.js
@@ -0,0 +1,9 @@
+function average(){
+  let num1=parseInt(window.prompt("enter 1st number"))
+  let num2=parseInt(window.prompt("enter 2nd number"))
+  let num3=parseInt(window.prompt("enter 3rd number"))
+  let result= (num1+num2+num3)/3;
+  window.prompt("your average is " + result)
+
+}
+average();
---
+++ parctice/imagechange.js
@@ -0,0 +1,6 @@
+function change(){
+    document.querySelector("#img").src="./img2.jpg"
+}
+function change2(){
+    document.querySelector("#img").src="./img1.jpg"
+}
