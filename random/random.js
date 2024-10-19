6da043941eb0b4704c083b4e45ca1c156f7be116
Manya Sharma <manya271001@gmail.com>
2024-10-19 12:14:03 +0530

    Math methods

---
+++ random/random.js
@@ -0,0 +1,11 @@
+let a = Math.PI;
+console.log(a);
+let b = Math.SQRT2;
+console.log(b);
+let c = 59.992;
+console.log(Math.round(c))
+console.log(Math.floor(c))
+console.log(Math.ceil(c))
+console.log(Math.abs(c)) // convert -ve number to positive
+console.log(Math.random())
+console.log(Math.pow(4,3))
