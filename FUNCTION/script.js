ac43e0e187c50471ae7552c91a3fdcb20d9097ec
Manya Sharma <manya271001@gmail.com>
2024-10-23 11:29:53 +0530

    function

---
+++ FUNCTION/script.js
@@ -0,0 +1,21 @@
+// regular function 
+function sum(){
+
+}
+sum();
+
+// arrow function: functions who are defined with the help of variable name
+const sum = ()=>{
+
+}
+run();
+
+// IIFE(Immediatly Invoked Function Expression ): invoked by itself
+(function(){
+
+})();
+
+// Higher order function: 
+setInterval(() => {
+    
+}, interval);
