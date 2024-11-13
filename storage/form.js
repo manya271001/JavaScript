e927f3533413e447e91158dd3d2dd1a7846d9048
Manya Sharma <manya271001@gmail.com>
2024-11-13 18:04:13 +0530

    storage

---
+++ storage/form.js
@@ -0,0 +1,16 @@
+function send(){
+    let enteredname = document.querySelector("#name").value
+    let enteredemail = document.querySelector("#email").value
+    let enteredcourse = document.querySelector("#course").value
+
+    localStorage.setItem("name",enteredname)
+    localStorage.setItem("email",enteredemail)
+    localStorage.setItem("course",enteredcourse)
+    
+    let a=localStorage.getItem("name")
+    let b=localStorage.getItem("email")
+    let c=localStorage.getItem("course")
+    document.querySelector("#first").innerHTML=a;
+    document.querySelector("#second").innerHTML=b;
+    document.querySelector("#third").innerHTML=c;
+}
---
+++ storage/local.js
@@ -0,0 +1,8 @@
+localStorage.setItem("name","manya")
+localStorage.setItem("age","23")
+localStorage.setItem("email","manya27@gmail.com")
+
+let name = localStorage.getItem("name")
+console.log(name);
+
+localStorage.removeItem("email")
