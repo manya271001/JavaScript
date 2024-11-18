1201ddcedf2d44323c619b066975df546e5dd275
Manya Sharma <manya271001@gmail.com>
2024-11-18 11:42:15 +0530

    import/export

---
+++ IMPORT/EXPORT/new.js
@@ -0,0 +1,7 @@
+function name1(){
+    console.log("first function called")
+}
+function name2(){
+    console.log("second function called")
+}
+export {name1,name2}
--- IMPORT/EXPORT/script.js
+++ IMPORT/EXPORT/script.js
@@ -1,2 +1,5 @@
 import add from "./add.js"
 console.log(add(10,20));
+import {name1,name2} from "./new.js"
+name1();
+name2();
