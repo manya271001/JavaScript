2d7a2e34face15e156f37198f3719e3c0e4fa145
Manya Sharma <manya271001@gmail.com>
2024-11-25 12:17:32 +0530

    slideUp and slideDown

--- jQuery/second.js
+++ jQuery/second.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
     $('.head1').html("hii allll")
+    $('.head1').css("background-color","blue")
+    // WE CAN APPLY MULTIPLE LINE CSS BY CREATING AN OBJECT
+    let a = $('.head2')
+    a.html("manyyaaa ")
+    a.css({
+        "background-color":"yellow",
+        "color":"red",
+        "padding":"20px"
+    })
+    $('#btn1').click(function(){
+        $('#para1').slideUp(2000);
+    })
+        $('#btn2').click(function(){
+        $('#para1').slideDown(2000);
+    })
 })
